22d9a3949b969dd5d9cd4358600b5b969fa595ae
Isadora Pilau <isadorapda@gmail.com>
2023-01-19 23:02:38 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <div></div>
-}
+import { Header } from './components/Header'
 
-export default App
+export function App() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-20">
+        <Header />
+        <div className="grid-rows-7"></div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Logo from '../assets/logo.svg'
+export function Header() {
+  return (
+    <div className="flex justify-between items-center px-10">
+      <img src={Logo} alt="Logo habit tracker" />
+      <button
+        type="button"
+        className="px-6 py-6 bg-transparent border border-violet-500 rounded-lg text-white text-center"
+      >
+        <span className="text-violet-500 pr-3">+</span> New Habit
+      </button>
+    </div>
+  )
+}
