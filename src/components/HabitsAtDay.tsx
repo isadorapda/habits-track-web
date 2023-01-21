b96cb8a9ce8de3cb686877de9530cdd014d17715
Isadora Pilau <isadorapda@gmail.com>
2023-01-21 01:00:13 -0300

    feat:UI popover daily habits

--- src/components/HabitsAtDay.tsx
+++ src/components/HabitsAtDay.tsx
@@ -1,5 +1,18 @@
+import * as Popover from '@radix-ui/react-popover'
+import { ProgressBar } from './ProgressBar'
+
 export function HabitsAtDay() {
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] bg-zinc-900 p-6 flex flex-col rounded-2xl">
+          <Popover.Arrow className="w-7 h-4 fill-zinc-900" />
+          <span className="text-zinc-400 font-semibold">monday</span>
+          <span className="text-3xl text-white font-extrabold mt-2">23/01</span>
+          <ProgressBar progress={66} />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,17 @@
+interface ProgressBarProps {
+  progress: number
+}
+
+export function ProgressBar({ progress }: ProgressBarProps) {
+  return (
+    <div className="h-3 bg-zinc-700 rounded-xl w-full mt-4 mb-6">
+      <div
+        className="h-3 bg-violet-600 rounded-xl absolute"
+        style={{ width: `${progress}%` }}
+        role="progressbar"
+        aria-label="Progress of habits completed in the selected date"
+        aria-aria-valuenow={progress}
+      />
+    </div>
+  )
+}
